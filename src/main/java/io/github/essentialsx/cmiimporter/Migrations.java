431c1ea619504df946a675464a5b1b3d33d515ed
darbyjack <admin@glaremasters.me>
2020-05-14 10:00:18 -0500

    Migrate the economy

--- src/main/java/io/github/essentialsx/cmiimporter/Migrations.java
+++ src/main/java/io/github/essentialsx/cmiimporter/Migrations.java
@@ -23,6 +23,7 @@ import co.aikar.idb.DbRow;
 import com.earth2me.essentials.Essentials;
 import com.earth2me.essentials.OfflinePlayer;
 import com.earth2me.essentials.User;
+import net.ess3.api.MaxMoneyException;
 import net.ess3.nms.refl.ReflUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -32,6 +33,7 @@ import org.bukkit.plugin.Plugin;
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.UUID;
@@ -53,6 +55,7 @@ public class Migrations {
         migrateHomes(ess);
         migrateNicknames(ess);
         migrateWarps(ess);
+        migrateEconomy(ess);
     }
 
     static void migrateUsers(Essentials ess) {
@@ -127,6 +130,20 @@ public class Migrations {
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    static void migrateEconomy(Essentials ess) {
+        try {
+            List<DbRow> results = DB.getResults("SELECT (player_uuid, Balance) FROM " + table("users"));
+            for (DbRow row : results) {
+                UUID uuid = UUID.fromString(row.getString("player_uuid"));
+                User user = ess.getUser(uuid);
+                Double money = row.getDbl("Balance", 0.0);
+                user.setMoney(BigDecimal.valueOf(money));
+            }
+        } catch (SQLException | MaxMoneyException ex) {
+            ex.printStackTrace();
         }
     }
 
